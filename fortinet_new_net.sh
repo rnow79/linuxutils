fc1555596ea4cb4b431b5e7bbd848ac5da5e4d20
Arnau Carrasco <arnau.carrasco@gmail.com>
2021-10-29 22:47:10 +0200

    forti net

---
+++ fortinet_new_net.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+NUM1="126 127"
+NUM2="`seq 1 254`"
+echo "config firewall ippool"
+for n1 in $NUM1 ; do
+ for n2 in $NUM2; do
+  cat << END
+ edit "Private Net $n1.$n2"
+ set startip 10.18.$n1.$n2
+ set endip 10.18.$n1.$n2
+ next
+END
+ done
+done
+
+echo end
+
